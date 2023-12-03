858b798b4a4df93a8d9db2c54da556948947e7c5
Nikhil Bolla <nikhilbolla2003@gmail.com>
2023-12-04 01:30:21 +0530

    logo change

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = () => {
     <Image
       onClick={() => router.push('/')}
       className="hidden md:block cursor-pointer" 
-      src="/images/logo.jpg" 
+      src="/images/logo.png" 
       height="100" 
       width="100" 
       alt="Logo" 
